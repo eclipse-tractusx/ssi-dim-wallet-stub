36c1448395bf7f8d7e584ec4f9b12021e93cc48d
Angel Yao <ayao@lksnext.com>
2025-06-02 09:24:58 +0200

    remove unused import

--- runtimes/ssi-dim-wallet-stub-memory/src/main/java/org/eclipse/tractusx/wallet/stub/runtime/memory/WalletStubApplication.java
+++ runtimes/ssi-dim-wallet-stub-memory/src/main/java/org/eclipse/tractusx/wallet/stub/runtime/memory/WalletStubApplication.java
@@ -30,8 +30,6 @@ import org.eclipse.tractusx.wallet.stub.config.rest.service.WalletStubRestServic
 import org.eclipse.tractusx.wallet.stub.runtime.memory.config.ConfigMarker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
